d778adb514767e0d3f318844f8c2e2960a2ed5fb
Avilagutier <mario.avila_gutierrez@protonmail.com>
2020-07-17 19:02:41 +0200

    first steps in R

---
+++ Control_Environ.R
@@ -0,0 +1,21 @@
+#Comentario individual
+"
+Forma de comentarios multilinea
+"
+"
+En estudios de estadística se llevaran a cabo varias modificaciones
+se necesitara mostrar el contenido de objetos. 
+Para mostrar la valor de un objeto x se hace por el comando 
+print
+"
+u = 6
+print(u)
+
+objects() #Da a conocer los objetos que se tienen en la sesion
+
+ls()# igual que objects
+
+#para eliminar uno se emplea la función rm: 
+rm(u)
+objects()
+
---
+++ Variables.R
@@ -0,0 +1,20 @@
+#Afectaciones
+x<-2 #creacciones de varialbes por la derecha e inzquierda
+X= 4
+3-> y
+
+objects() #tipos de objetos
+rm(x) #para quitar un elemento
+length(X) # tipo de longitud
+mode(X) #tipo de variable
+
+# Afectacion es asociar un valor o un objeto en particular a un nombre
+# en si es la creacion de una varialbe 
+
+# operadores : <-, -> ou =
+
+z = 2
+Z <-5
+
+data(iris)
+
